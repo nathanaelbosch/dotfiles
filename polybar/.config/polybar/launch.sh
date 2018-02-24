2806416c33c0cdb356853b182e4d3815d05d3bed
Nathanael Bosch <nathanael.bosch@tum.de>
2018-02-24 11:33:56 +0100

    different bar for the laptop!

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -6,11 +6,16 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch bar1 and bar2
-polybar bottom_main &
-polybar bottom_left &
-polybar bottom_right &
-# polybar top &
+
+# Make the launched bar dependent on the host name!
+if [ "$HOSTNAME" = nath-pc ]; then
+    polybar bottom_main &
+    polybar bottom_left &
+    polybar bottom_right &
+    # polybar top &
+elif [ "$HOSTNAME" = nath-laptop ]; then
+    polybar bottom_laptop &
+fi
 
 
 echo "Bars launched..."
