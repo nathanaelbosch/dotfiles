be6b2acb5e1758acf5bf9336a2302d32d34a571d
Nathanael Bosch <nathanael.bosch@tum.de>
2018-07-05 17:08:34 +0200

    very useful makro; quickly go to project

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,3 +2,12 @@ abbr -a -- - 'cd -'
 if test -e ~/.aliases
     source ~/.aliases
 end
+
+# goto makro to quickly open any Project + venv
+function gt
+    set PROJECT_NAME $argv
+    echo "Going to $PROJECT_NAME"
+    cd ~/Projekte/$PROJECT_NAME
+    echo "Activating venv"
+    . .venv/bin/activate.fish
+end
