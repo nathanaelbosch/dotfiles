2888c26e45f89bf1cd255916037b7935db1698a4
Nathanael Bosch <nathanael.bosch@tum.de>
2018-07-05 17:44:23 +0200

    better now, and brand new project creation makro, but this is probably overkill and should be handled by a script

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -4,10 +4,33 @@ if test -e ~/.aliases
 end
 
 # goto makro to quickly open any Project + venv
-function gt
+function project -d "Go to project and activate venv"
     set PROJECT_NAME $argv
-    echo "Going to $PROJECT_NAME"
+    # echo "Going to $PROJECT_NAME"
     cd ~/Projekte/$PROJECT_NAME
-    echo "Activating venv"
-    . .venv/bin/activate.fish
+    if test $status = 0; and test -d .venv
+        echo "Activating venv"
+        . .venv/bin/activate.fish
+    end
 end
+
+function new_project
+    set PROJECT_NAME $argv
+    set PROJECT_PATH "/home/nath/Projekte/$PROJECT_NAME"
+    echo "Path: $PROJECT_PATH"
+    if not test -d $PROJECT_PATH
+        echo "Initializing Project"
+        mkdir $PROJECT_PATH
+        cd $PROJECT_PATH
+        python -m venv .venv
+        . .venv/bin/activate.fish
+        # touch requirements.txt
+        # # touch test_sample.py
+        # # mkdir $PROJECT_NAME
+        # # touch README.md
+    else
+        echo "Project exists already!"
+        return 1
+    end
+end
+
