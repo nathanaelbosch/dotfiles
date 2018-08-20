c816b3868f51fe68f3660f0d14ec16b10434ef2e
Nathanael Bosch <nathanael.bosch@tum.de>
2018-08-20 12:05:16 +0200

    added qutebrowser

---
+++ dotfiles/config/qutebrowser/config.py
@@ -0,0 +1,18 @@
+config.load_autoconfig()
+
+BG = '#1d1f21'
+FG = '#c5c8c6'
+DARK_BLUE = '#5f819d'
+DARK_GREEN = '#8c9440'
+
+
+
+#c.colors.statusbar.normal.bg = BG
+#c.colors.statusbar.normal.fg = FG
+#c.colors.statusbar.insert.bg = DARK_GREEN
+#c.colors.statusbar.insert.fg = FG
+#c.colors.statusbar.passthrough.bg = DARK_BLUE
+#c.colors.statusbar.passthrough.fg = FG
+#c.colors.statusbar.command.bg = BG
+#c.colors.statusbar.command.fg = FG
+#c.colors.statusbar.url.fg = FG
