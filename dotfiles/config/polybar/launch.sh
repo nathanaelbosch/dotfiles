c3772e1f3f26667e2d6746c3c3cd0bb0e11ed887
Nathanael Bosch <nathanael.bosch@tum.de>
2018-11-06 07:21:28 +0100

    Polybar is now smart if only one screen is active
    A script to disable all secondary screens is still missing, and that would
    definitely be a nice feature

--- dotfiles/config/polybar/launch.sh
+++ dotfiles/config/polybar/launch.sh
@@ -9,10 +9,21 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Make the launched bar dependent on the host name!
 if [ "$HOSTNAME" = nath-pc ]; then
-    polybar bottom_main &
-    polybar bottom_left &
-    polybar bottom_right &
-    # polybar top &
+    i=0
+    for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+        case $i in
+            0)
+                polybar bottom_main &;;
+            1)
+                polybar bottom_left &;;
+            2)
+                polybar bottom_right &;;
+            *)
+                # polybar top &
+        esac
+        i=$((i + 1))
+        echo $i
+    done
 elif [ "$HOSTNAME" = nath-laptop ]; then
     polybar bottom_laptop &
 fi
