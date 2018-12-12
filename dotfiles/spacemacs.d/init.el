962dbb801ef80a13750ae865adbf8be6094cb828
Nathanael Bosch <nathanael.bosch@tum.de>
2018-12-12 10:04:26 +0100

    Spacemacs: Added yasnippets-snippets

--- dotfiles/spacemacs.d/init.el
+++ dotfiles/spacemacs.d/init.el
@@ -82,7 +82,8 @@ This function should only modify configuration layer settings."
    ;; To use a local version of a package, use the `:location' property:
    ;; '(your-package :location "~/path/to/your-package/")
    ;; Also include the dependencies as they will not be resolved automatically.
-   dotspacemacs-additional-packages '(interleave)
+   dotspacemacs-additional-packages '(interleave
+                                      yasnippet-snippets)
 
    ;; A list of packages that cannot be updated.
    dotspacemacs-frozen-packages '()
