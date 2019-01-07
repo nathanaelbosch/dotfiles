956263cede25c57a323bc94ac651fa7ed326b7da
Nathanael Bosch <nathanael.bosch@tum.de>
2019-01-07 08:06:14 +0100

    Spacemacs:
    - Function to setup google calendar so that the file gets more readable
    
    - Copied FiraCode related stuff from @PierreSp, but I don't use it yet
    
    - Better org agenda views
    
    - Better org source blocks

--- dotfiles/spacemacs.d/init.el
+++ dotfiles/spacemacs.d/init.el
@@ -497,6 +497,23 @@ If you are unsure, try setting them in `dotspacemacs/user-config' first."
   (setq explicit-shell-file-name "/bin/fish")
   (setq shell-file-name "fish")
 
+  (setup-google-calendar)
+
+  ;; Org babel languages
+  (org-babel-do-load-languages
+   'org-babel-load-languages
+   '((C . t)
+     (python . t)
+     (gnuplot . t)
+     ))
+
+  (add-to-list 'org-modules 'habits)
+  )
+
+
+(defun setup-google-calendar ()
+  ;; Wrapper function to setup my google calendar with org
+
   ;; Function to read lines of a file and output a list
   (defun read-lines (filePath)
     "Return a list of lines of a file at filePath."
@@ -508,15 +525,145 @@ If you are unsure, try setting them in `dotspacemacs/user-config' first."
   ;; Google Calendar
   (setq org-gcal-client-id (car auth-lines)
         org-gcal-client-secret (car (cdr auth-lines)))
-
-  ;; Org babel languages
-  (org-babel-do-load-languages
-   'org-babel-load-languages
-   '((C . t)
-     (python . t)
-     (gnuplot . t)
-     ))
   )
+
+
+(defun setup-fira-code ()
+  ;; Fira code stuff - copied from PierreSp
+  ;; This works when using emacs --daemon + emacsclient
+  (add-hook 'after-make-frame-functions (lambda (frame) (set-fontset-font t '(#Xe100 . #Xe16f) "Fira Code Symbol")))
+  ;; This works when using emacs without server/client
+  (set-fontset-font t '(#Xe100 . #Xe16f) "Fira Code Symbol")
+  ;; I haven't found one statement that makes both of the above situations work, so I use both for now
+
+  (defconst fira-code-font-lock-keywords-alist
+    (mapcar (lambda (regex-char-pair)
+              `(,(car regex-char-pair)
+                (0 (prog1 ()
+                     (compose-region (match-beginning 1)
+                                     (match-end 1)
+                                     ;; The first argument to concat is a string containing a literal tab
+                                     ,(concat "	" (list (decode-char 'ucs (cadr regex-char-pair)))))))))
+            '(("\\(www\\)"                   #Xe100)
+              ("[^/]\\(\\*\\*\\)[^/]"        #Xe101)
+              ("\\(\\*\\*\\*\\)"             #Xe102)
+              ("\\(\\*\\*/\\)"               #Xe103)
+              ("\\(\\*>\\)"                  #Xe104)
+              ("[^*]\\(\\*/\\)"              #Xe105)
+              ("\\(\\\\\\\\\\)"              #Xe106)
+              ("\\(\\\\\\\\\\\\\\)"          #Xe107)
+              ("\\({-\\)"                    #Xe108)
+              ;; ("\\(\\[\\]\\)"                #Xe109) This is the [] ligature and I don't like
+              ("\\(::\\)"                    #Xe10a)
+              ("\\(:::\\)"                   #Xe10b)
+              ("[^=]\\(:=\\)"                #Xe10c)
+              ("\\(!!\\)"                    #Xe10d)
+              ("\\(!=\\)"                    #Xe10e)
+              ("\\(!==\\)"                   #Xe10f)
+              ("\\(-}\\)"                    #Xe110)
+              ("\\(--\\)"                    #Xe111)
+              ("\\(---\\)"                   #Xe112)
+              ("\\(-->\\)"                   #Xe113)
+              ("[^-]\\(->\\)"                #Xe114)
+              ("\\(->>\\)"                   #Xe115)
+              ("\\(-<\\)"                    #Xe116)
+              ("\\(-<<\\)"                   #Xe117)
+              ("\\(-~\\)"                    #Xe118)
+              ("\\(#{\\)"                    #Xe119)
+              ("\\(#\\[\\)"                  #Xe11a)
+              ("\\(##\\)"                    #Xe11b)
+              ("\\(###\\)"                   #Xe11c)
+              ("\\(####\\)"                  #Xe11d)
+              ("\\(#(\\)"                    #Xe11e)
+              ("\\(#\\?\\)"                  #Xe11f)
+              ("\\(#_\\)"                    #Xe120)
+              ("\\(#_(\\)"                   #Xe121)
+              ("\\(\\.-\\)"                  #Xe122)
+              ("\\(\\.=\\)"                  #Xe123)
+              ("\\(\\.\\.\\)"                #Xe124)
+              ("\\(\\.\\.<\\)"               #Xe125)
+              ("\\(\\.\\.\\.\\)"             #Xe126)
+              ("\\(\\?=\\)"                  #Xe127)
+              ("\\(\\?\\?\\)"                #Xe128)
+              ("\\(;;\\)"                    #Xe129)
+              ("\\(/\\*\\)"                  #Xe12a)
+              ("\\(/\\*\\*\\)"               #Xe12b)
+              ("\\(/=\\)"                    #Xe12c)
+              ("\\(/==\\)"                   #Xe12d)
+              ("\\(/>\\)"                    #Xe12e)
+              ("\\(//\\)"                    #Xe12f)
+              ("\\(///\\)"                   #Xe130)
+              ("\\(&&\\)"                    #Xe131)
+              ("\\(||\\)"                    #Xe132)
+              ("\\(||=\\)"                   #Xe133)
+                                        ;("[^|]\\(|=\\)"                #Xe134)
+              ("\\(|>\\)"                    #Xe135)
+              ("\\(\\^=\\)"                  #Xe136)
+              ("\\(\\$>\\)"                  #Xe137)
+              ("\\(\\+\\+\\)"                #Xe138)
+              ("\\(\\+\\+\\+\\)"             #Xe139)
+              ("\\(\\+>\\)"                  #Xe13a)
+              ("\\(=:=\\)"                   #Xe13b)
+                                        ;("[^!/]\\(==\\)[^>]"           #Xe13c)
+              ("\\(===\\)"                   #Xe13d)
+              ("\\(==>\\)"                   #Xe13e)
+                                        ;("[^=]\\(=>\\)"                #Xe13f)
+              ("\\(=>>\\)"                   #Xe140)
+              ("\\(<=\\)"                    #Xe141)
+              ("\\(=<<\\)"                   #Xe142)
+              ("\\(=/=\\)"                   #Xe143)
+              ("\\(>-\\)"                    #Xe144)
+              ("\\(>=\\)"                    #Xe145)
+              ("\\(>=>\\)"                   #Xe146)
+              ("[^-=]\\(>>\\)"               #Xe147)
+              ("\\(>>-\\)"                   #Xe148)
+              ("\\(>>=\\)"                   #Xe149)
+              ("\\(>>>\\)"                   #Xe14a)
+              ("\\(<\\*\\)"                  #Xe14b)
+              ("\\(<\\*>\\)"                 #Xe14c)
+              ("\\(<|\\)"                    #Xe14d)
+              ("\\(<|>\\)"                   #Xe14e)
+              ("\\(<\\$\\)"                  #Xe14f)
+              ("\\(<\\$>\\)"                 #Xe150)
+              ("\\(<!--\\)"                  #Xe151)
+              ("\\(<-\\)"                    #Xe152)
+              ("\\(<--\\)"                   #Xe153)
+              ("\\(<->\\)"                   #Xe154)
+              ("\\(<\\+\\)"                  #Xe155)
+              ("\\(<\\+>\\)"                 #Xe156)
+              ("\\(<=\\)"                    #Xe157)
+              ("\\(<==\\)"                   #Xe158)
+              ("\\(<=>\\)"                   #Xe159)
+              ("\\(<=<\\)"                   #Xe15a)
+              ("\\(<>\\)"                    #Xe15b)
+              ("[^-=]\\(<<\\)"               #Xe15c)
+              ("\\(<<-\\)"                   #Xe15d)
+              ("\\(<<=\\)"                   #Xe15e)
+              ("\\(<<<\\)"                   #Xe15f)
+              ("\\(<~\\)"                    #Xe160)
+              ("\\(<~~\\)"                   #Xe161)
+              ("\\(</\\)"                    #Xe162)
+              ("\\(</>\\)"                   #Xe163)
+              ("\\(~@\\)"                    #Xe164)
+              ("\\(~-\\)"                    #Xe165)
+              ("\\(~=\\)"                    #Xe166)
+              ("\\(~>\\)"                    #Xe167)
+              ("[^<]\\(~~\\)"                #Xe168)
+              ("\\(~~>\\)"                   #Xe169)
+              ("\\(%%\\)"                    #Xe16a)
+              ;; ("\\(x\\)"                   #Xe16b) This ended up being hard to do properly so i'm leaving it out.
+              ("[^:=]\\(:\\)[^:=]"           #Xe16c)
+              ("[^\\+<>]\\(\\+\\)[^\\+<>]"   #Xe16d)
+              ("[^\\*/<>]\\(\\*\\)[^\\*/<>]" #Xe16f)
+              )))
+
+  (defun add-fira-code-symbol-keywords ()
+    (font-lock-add-keywords nil fira-code-font-lock-keywords-alist))
+
+  (add-hook 'prog-mode-hook
+            #'add-fira-code-symbol-keywords)
+  )
+
 
 (defun org-ref-config ()
   ;; Followed https://codearsonist.com/reading-for-programmers and https://github.com/jkitchin/org-ref
@@ -547,6 +694,7 @@ If you are unsure, try setting them in `dotspacemacs/user-config' first."
   (evil-leader/set-key-for-mode 'org-mode "B" 'helm-bibtex)
   (evil-leader/set-key-for-mode 'bibtex-mode "B" 'helm-bibtex)
   )
+
 
 (defun my-org-config()
 
@@ -624,37 +772,47 @@ If you are unsure, try setting them in `dotspacemacs/user-config' first."
                     (org-agenda-skip-scheduled-if-done nil)
                     (org-agenda-log-mode)
                     ))
-                  ;; ("gn" "Next Actions" tags-todo "TODO"
-                  ;;  ((org-agenda-filter-preset '("-someday"))
-                  ;;   ;; (org-agenda-todo-ignore-with-date t)
-                  ;;   ))
-                  ;; ("gd" "DONE" tags-todo "DONE" ((org-use-tag-inheritance nil)))
-                  ;; ("gs" "SOMEDAY" tags "someday"
-                  ;;  ((org-agenda-filter-preset '("+someday"))
-                  ;;   (org-agenda-todo-ignore-with-date nil)))
-                  ;; ("gs" "SOMEDAY" tags "someday" ((org-use-tag-inheritance nil)))
 
-                  ("n" "Next Actions" todo "TODO"
-                   ((org-agenda-filter-preset '("-someday"))
-                    (org-agenda-todo-ignore-scheduled t)
-                    (org-deadline-warning-days 90)
+                  ("n" "Next Actions"
+                   ((todo "TODO"
+                    ((org-agenda-filter-preset '("-someday"))
+                     (org-agenda-todo-ignore-scheduled t)
+                     (org-deadline-warning-days 90)))
+                    (agenda ""
+                     ((org-agenda-entry-types '(:deadline))
+                      (org-agenda-span 1)
+                      (org-deadline-warning-days 90)
+                      (org-agenda-time-grid nil)
+                      (org-agenda-overriding-header "Upcoming Deadlines")
+                      ))
                     ))
+
+                  ;; Special types
                   ("s" "Someday" tags "someday"
                    ((org-agenda-filter-preset '("+someday"))
                     (org-agenda-todo-ignore-with-date nil)))
                   ("r" "To read" todo "TOREAD"
-                   ((org-agenda-filter-preset '("+someday"))))
+                   ((org-agenda-filter-preset '(""))))
                   ("w" "To watch" todo "TOWATCH"
-                   ((org-agenda-filter-preset '("+someday"))))
+                   ((org-agenda-filter-preset '(""))
+                    (org-agenda-view-columns-initially t)
+                    ))
 
-                  ("c" . "Contexts")
-                  ("ch" "@Home" tags-todo "@home")
-                  ("cu" "@Uni" tags-todo "@uni")
+                  ;; Contexts
+                  ("h" "@Home"
+                   ((tags "@home"
+                         ((org-agenda-todo-ignore-with-date nil)))
+                   ))
+                  ("u" "@Uni"
+                   ((tags "@uni"
+                          ((org-agenda-todo-ignore-with-date nil)))
+                    ))
 
                   ("d" "Upcoming deadlines" agenda ""
                    ((org-agenda-entry-types '(:deadline))
                     (org-agenda-span 1)
                     (org-deadline-warning-days 60)
+                    (org-agenda-overriding-header "Upcoming Deadlines")
                     (org-agenda-time-grid nil)))
 
                   )
