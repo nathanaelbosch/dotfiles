5ebe1aa6a870a8f15e4eb4f67e10e0c49bfddb97
Nathanael Bosch <nathanael.bosch@tum.de>
2020-05-10 13:23:31 +0200

    Spacemacs: org-roam, but currently not working

--- dotfiles/spacemacs.d/layers/org-roam/packages.el
+++ dotfiles/spacemacs.d/layers/org-roam/packages.el
@@ -1,27 +1,26 @@
 (defconst org-roam-packages
-  '((org-roam :location
-        (recipe :fetcher github :repo "jethrokuan/org-roam" :branch "master"))))
+  '(org-roam))
 
 (defun org-roam/init-org-roam ()
-    (use-package org-roam
-        :hook
-        (after-init . org-roam-mode)
-        :custom
-        (org-roam-directory "~/Dropbox/org/zettelkasten/")
-        :init
-        (progn
-          (spacemacs/declare-prefix "aoR" "org-roam")
-          (spacemacs/set-leader-keys
-            "aoRl" 'org-roam
-            "aoRt" 'org-roam-today
-            "aoRf" 'org-roam-find-file
-            "aoRg" 'org-roam-show-graph)
+  (use-package org-roam
+    :hook
+    (after-init . org-roam-mode)
+    :custom
+    (org-roam-directory "~/Dropbox/org/zettelkasten/")
+    :init
+    (progn
+      (spacemacs/declare-prefix "aoR" "org-roam")
+      (spacemacs/set-leader-keys
+        "aoRl" 'org-roam
+        "aoRt" 'org-roam-today
+        "aoRf" 'org-roam-find-file
+        "aoRg" 'org-roam-show-graph)
 
-          (spacemacs/declare-prefix-for-mode 'org-mode "mr" "org-roam")
-          (spacemacs/set-leader-keys-for-major-mode 'org-mode
-            "rl" 'org-roam
-            "rt" 'org-roam-today
-            "rb" 'org-roam-switch-to-buffer
-            "rf" 'org-roam-find-file
-            "ri" 'org-roam-insert
-            "rg" 'org-roam-show-graph))))
+      (spacemacs/declare-prefix-for-mode 'org-mode "mr" "org-roam")
+      (spacemacs/set-leader-keys-for-major-mode 'org-mode
+        "rl" 'org-roam
+        "rt" 'org-roam-today
+        "rb" 'org-roam-switch-to-buffer
+        "rf" 'org-roam-find-file
+        "ri" 'org-roam-insert
+        "rg" 'org-roam-show-graph))))
