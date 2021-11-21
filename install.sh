57d5fe0166e9dc6a9813aa5a37fbcb80c24299c3
Nathanael Bosch <nathanael.bosch@gmail.com>
2021-11-21 17:18:17 +0100

    Update the install script

--- install.sh
+++ install.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 
-# Clone the dotfile repo
+echo "Clone the dotfiles repo into ~/dotfiles"
 git clone --recursive https://github.com/nathbo/dotfiles.git ~/dotfiles
 cd ~/dotfiles
 
-# Install the dotdrop requirements
+echo "Install the dotdrop requirements into a new venv"
+echo ""
 python -m venv .venv
 . .venv/bin/activate
 pip install --user -r dotdrop/requirements.txt
 
 # Install the dotfiles
+echo "Install the dotfiles (if the hostname exists in the config file)"
 ./dotdrop.sh install
