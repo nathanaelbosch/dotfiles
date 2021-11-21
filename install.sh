ac450e1b1b9f167997dc2c110779c8d3991c3efc
Nathanael Bosch <nathanael.bosch@gmail.com>
2021-11-21 17:56:28 +0100

    Make the install.sh script more verbose

--- install.sh
+++ install.sh
@@ -1,15 +1,26 @@
 #!/bin/sh
 
-echo "Clone the dotfiles repo into ~/dotfiles"
+echo "
+################################################################################
+# Clone the dotfiles repo into ~/dotfiles
+################################################################################
+"
 git clone --recursive https://github.com/nathbo/dotfiles.git ~/dotfiles
 cd ~/dotfiles
 
-echo "Install the dotdrop requirements into a new venv"
-echo ""
+echo "
+################################################################################
+# Install the dotdrop requirements into a new venv
+################################################################################
+"
 python -m venv .venv
 . .venv/bin/activate
-pip install --user -r dotdrop/requirements.txt
+pip install -r dotdrop/requirements.txt
 
 # Install the dotfiles
-echo "Install the dotfiles (if the hostname exists in the config file)"
+echo "
+################################################################################
+# Install the dotfiles (if the hostname exists in the config file)
+################################################################################
+"
 ./dotdrop.sh install
