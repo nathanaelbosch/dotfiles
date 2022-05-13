64193d82352008dec5632e7a0e853dca1bf9b39f
Nathanael Bosch <nathanael.bosch@uni-tuebingen.de>
2022-05-13 09:41:34 +0300

    Add the julia startup file

---
+++ dotfiles/julia/config/startup.jl
@@ -0,0 +1,32 @@
+ENV["EDITOR"] = "vim"
+# ENV["PYTHON"] = "python"
+
+function activate_default_packages()
+    @eval using Revise
+    @eval using OhMyREPL
+    @eval colorscheme!("GruvboxDark")
+    @eval using AbbreviatedStackTraces
+    @eval ENV["JULIA_STACKTRACE_MINIMAL"] = true
+    @eval using BenchmarkTools
+    @eval using Logging: global_logger
+    @eval using TerminalLoggers
+    @eval global_logger(TerminalLogger())
+    @eval using Eyeball
+end
+
+try
+    activate_default_packages()
+catch err
+    @error "Could not load startup packages. To install, use `_setup()`" err
+end
+
+function _setup()
+    @eval using Pkg
+    Pkg.add("Revise")
+    Pkg.add("OhMyREPL")
+    Pkg.add("BenchmarkTools")
+    Pkg.add("TerminalLoggers")
+    Pkg.add(url="https://github.com/BioTurboNick/AbbreviatedStackTraces.jl")
+    Pkg.add("Eyeball")
+    activate_default_packages()
+end
