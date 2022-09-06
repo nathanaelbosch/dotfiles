af405ad92fd24708012e35599276a54b81710657
Nathanael Bosch <nathanael.bosch@uni-tuebingen.de>
2022-09-06 09:20:47 +0200

    Update the uni screenlayout

--- dotfiles/screenlayout/uni_layout.sh
+++ dotfiles/screenlayout/uni_layout.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 xrandr --output DVI-D-0 --off \
-       --output HDMI-0 --primary --mode 3840x2160 --pos 0x1680 --rotate normal \
-       --output DP-5 --off \
-       --output DP-4 --mode 3840x2160 --pos 3840x0 --rotate right \
-       --output DP-3 --off \
-       --output DP-2 --off \
+       --output HDMI-0 --mode 3840x2160 --pos 0x1061 --rotate normal --primary \
+       --output DP-0 --off \
        --output DP-1 --off \
-       --output DP-0 --off
+       --output DP-2 --off \
+       --output DP-3 --off \
+       --output DP-4 --mode 4096x2160 --pos 3840x0 --rotate right \
+       --output DP-5 --off
