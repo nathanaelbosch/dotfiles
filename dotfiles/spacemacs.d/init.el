4fec14b6f786e137be2a9bf5ebb60024192fa3a9
Nathanael Bosch <nathanael.bosch@uni-tuebingen.de>
2023-08-29 14:58:25 +0300

    [spacemacs] update email config - FIX BUG!

--- dotfiles/spacemacs.d/init.el
+++ dotfiles/spacemacs.d/init.el
@@ -81,11 +81,11 @@ This function should only modify configuration layer settings."
            {%@@ if profile == "murr" @@%}
            mu4e-installation-path "/usr/local/share/emacs/site-lisp/mu4e"
            {%@@ elif distro_like == "ubuntu debian" @@%}
-           mu4e-installation-path "/usr/share/emacs/site-lisp/mu4e"
+           mu4e-installation-path "/snap/maildir-utils/current/share/emacs/site-lisp/mu4e"
            {%@@ else @@%}
            mu4e-installation-path "/usr/share/emacs/site-lisp/mu4e"
            {%@@ endif @@%}
-           mu4e-use-maildirs-extension t
+           mu4e-use-maildirs-extension nil
            mu4e-maildir "~/Mail"
            mu4e-enable-mode-line t
            mu4e-get-mail-command "offlineimap"
@@ -96,7 +96,7 @@ This function should only modify configuration layer settings."
            mu4e-confirm-quit nil
            mu4e-enable-notifications t
            mu4e-headers-skip-duplicates t
-           mu4e-enable-async-operations t
+           mu4e-enable-async-operations nil
            )
      ;;(multiple-cursors :variables multiple-cursors-backend 'evil-mc)
      neotree
