f2c926c15a12307303af9bd8be1b493e39736d05
Nathanael Bosch <nathanael.bosch@uni-tuebingen.de>
2023-10-03 14:00:28 +0200

    spacemacs: make scratch an org-mode buffer

--- dotfiles/spacemacs.d/init.el
+++ dotfiles/spacemacs.d/init.el
@@ -310,11 +310,13 @@ It should only modify the values of Spacemacs settings."
    dotspacemacs-new-empty-buffer-major-mode 'text-mode
 
    ;; Default major mode of the scratch buffer (default `text-mode')
-   dotspacemacs-scratch-mode 'text-mode
+   dotspacemacs-scratch-mode 'org-mode
 
    ;; Initial message in the scratch buffer, such as "Welcome to Spacemacs!"
    ;; (default nil)
-   dotspacemacs-initial-scratch-message nil
+   dotspacemacs-initial-scratch-message "#+TITLE: Scratch
+
+"
 
    ;; List of themes, the first of the list is loaded when spacemacs starts.
    ;; Press `SPC T n' to cycle to the next theme in the list (works great
